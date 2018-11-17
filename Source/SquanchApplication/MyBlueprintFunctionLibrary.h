6983fc5ec1a0e8f5b0d4c8b98ae4d347ec51b2ee
davidkol <davidkolop@gmail.com>
2018-11-17 14:03:18 -0800

    1.05 Added Plate and Mug as destructable and holdable objects

---
+++ Source/SquanchApplication/MyBlueprintFunctionLibrary.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+
+#include "MyBlueprintFunctionLibrary.generated.h"
+/**
+ * 
+ */
+UCLASS()
+class SQUANCHAPPLICATION_API UMyBlueprintFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+// 		UFUNCTION(BlueprintCallable, Category="ChromaKey")
+// 		static float3 ExtractColor(float3 Color, float LumaMask);
+	
+};
